1c02b04e1b169cff5507f10dd6796e9a00f0bc56
Arin Cantika <arincantikam@gmail.com>
2023-06-01 16:35:57 +0700

    update home produk list

--- env_ku/Scripts/activate.bat
+++ env_ku/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\STUDY\womeintech-python-django\hands_on\ecommerce-all_auth\env_arin
+set VIRTUAL_ENV=D:\STUDY\womeintech-python-django\hands_on\Ecommerce_B7\env_ku
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(env_arin) %PROMPT%
+set PROMPT=(env_ku) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,6 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(env_arin) 
 
 :END
 if defined _OLD_CODEPAGE (
